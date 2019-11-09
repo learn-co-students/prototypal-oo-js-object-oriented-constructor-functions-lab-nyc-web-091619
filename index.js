095fe82042b464ff7f616c85a37d04fc902e346d
Michelle Lee <mlee092@Michelles-MacBook.local>
2019-11-09 15:22:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+
+// Scooter constructor
+// ✓ should create a new scooter with three properties
 function Scooter(year, color, model) {
     this.year= year;
     this.color = color;
     this.model = model;
     
 }
+
+// Driver constructor
+// ✓ should create a new driver with three properties
 function Driver(name, age, experience) {
     this.name= name;
     this.age = age;
     this.experience = experience;
     
 }
+
+// PickupLocation constructor
+// ✓ should create a new PickupLocation with two properties
 function PickupLocation(address, city) {
     this.address= address;
     this.city = city;
